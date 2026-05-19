94451556a07f5dad59027176175b7e587ef33167
RicardoFernandes99 <filipericardo7@hotmail.com>
2026-05-19 11:40:28 +0100

    more random logic checks

--- RandomExtraTesting/randomLogic.jsx
+++ RandomExtraTesting/randomLogic.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const productsMock = [
     { id: 1, name: "Keyboard", price: 50, favorite: true },
     { id: 2, name: "Mouse", price: 20, favorite: false },
@@ -40,6 +42,49 @@ function ProductsPage() {
                     </li>
                 ))}
             </ul>
+        </>
+    );
+}
+
+function MyProdPage() {
+    const [products, setProducts] = useState(productsMock);
+    const [search, setSearch] = useState("");
+    const [newName, setNewName] = useState("");
+    const filterProds = (search) => {
+        return products.filter((p) =>
+            p.name.toLowerCase().includes(search.toLowerCase()),
+        );
+    };
+    const filterProducts = filterProds(search);
+
+    const handleDelete = (id) => {
+        setProducts((prev) => prev.filter((p) => p.id !== id));
+    };
+    const update = (id, newname) => {
+        setProducts((prev) =>
+            prev.map((p) => (p.id === id ? { ...p, name: newname } : p)),
+        );
+    };
+    return (
+        <>
+            <input
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+            ></input>
+
+            {filterProducts.map((p) => (
+                <div>
+                    <li key={p.id}> {p.name}</li>
+                    <button onClick={() => handleDelete(p.id)}>Delete</button>
+                    <input
+                        value={newName}
+                        onChange={(e) => setNewName(e.target.value)}
+                    ></input>
+                    <button onClick={() => handleUpdate(p.id, newName)}>
+                        Delete
+                    </button>
+                </div>
+            ))}
         </>
     );
 }
